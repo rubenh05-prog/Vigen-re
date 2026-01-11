66d7fbbc217d28764af1c6a9928a4bd584c25b2f
Robin <rubenh05@googlemail.com>
2026-01-11 14:02:52 +0100

    service-worker.js aktualisieren

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 /* 
   Service Worker – Additive Unicode-Chiffre (Vigenère-Prinzip)
-  Version: v2
+  Version: v2.4
 */
 
-const CACHE_NAME = "vigenere-pwa-final-c-v2.3";
+const CACHE_NAME = "vigenere-pwa-final-c-v2.4";
 
 const FILES_TO_CACHE = [
   "./",
