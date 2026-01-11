19c6cd7c69b137e1f740bf7ea60c037bc08cbdfd
Robin <rubenh05@googlemail.com>
2026-01-11 12:12:50 +0100

    Update cache version in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="vigenere-pwa-final-c-v1";
+const CACHE="vigenere-pwa-final-c-v2";
 const FILES=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));
